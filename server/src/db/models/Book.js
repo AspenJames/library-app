006b14f07f8fc3edd59b9c98061ba3816a8af9d8
Aspen James <aspenjames@tqca.org>
2022-12-13 20:38:31 -0800

    Add Book model file

---
+++ server/src/db/models/Book.js
@@ -0,0 +1,47 @@
+import { Model } from 'objection';
+
+import { boolType, stringType } from './_schemaTypes';
+
+class Book extends Model {
+  static get tableName() {
+    return 'books'; // 20221212010204_create-books.mjs
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  static get jsonSchema() {
+    return {
+        type: 'object',
+        required: ['title'],
+        properties: {
+            id: stringType({ format: 'uuid' }),
+            isbn: stringType(),
+            title: stringType({ minLength: 1 }),
+            edition: stringType(),
+            readStatus: boolType()
+        }
+    }
+  }
+
+  static get relationMappings() {
+    return {
+        // TODO
+        // authors: {
+        //     relation: Model.ManyToManyRelation,
+        //     modelClass: Author,
+        //     join: {
+        //         from: 'books.id',
+        //         through: {
+        //             from: 'book_authors.book_id',
+        //             to: 'book_authors.author_id',
+        //         },
+        //         to: 'authors.id'
+        //     }
+        // }
+    }
+  }
+}
+
+export default Book;

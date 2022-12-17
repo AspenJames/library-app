8c48655aa7b23c4dd23abcf1e4fbd89b774ec8bc
Aspen James <hello@aspenjames.dev>
2022-12-17 12:14:13 -0800

    Add uuid default; update constraints
    Editing the migration files directly, since we can just blow out the
    local db container/volume & re-migrate fresh

--- server/src/db/migrations/20221212010204_create-books.mjs
+++ server/src/db/migrations/20221212010204_create-books.mjs
@@ -1,16 +1,17 @@
 // npm run knex migrate:make create-books
 export const up = async (knex) =>
   knex.schema.createTable("books", (table) => {
-    table.uuid("id", { primaryKey: true });
+    table.uuid("id", { primaryKey: true })
+      .defaultTo(knex.raw('gen_random_uuid()'));
     table.timestamps({
       useTimestamps: true,
       defaultToNow: true,
       useCamelCase: false,
     });
     table.string("isbn", 255);
-    table.string("title", 255);
+    table.string("title", 255).notNullable();
     table.string("edition", 255);
-    table.boolean("read_status");
+    table.boolean("read_status").defaultTo(false);
   });
 
 export const down = async (knex) => knex.schema.dropTableIfExists("books");
--- server/src/db/migrations/20221214044901_create-authors.mjs
+++ server/src/db/migrations/20221214044901_create-authors.mjs
@@ -1,13 +1,14 @@
 // npm run knex migrate:make create-authors
 export const up = async (knex) =>
   knex.schema.createTable("authors", (table) => {
-    table.uuid("id", { primaryKey: true });
+    table.uuid("id", { primaryKey: true })
+      .defaultTo(knex.raw('gen_random_uuid()'));
     table.timestamps({
       useTimestamps: true,
       defaultToNow: true,
       useCamelCase: false,
     });
-    table.string("name", 255);
+    table.string("name", 255).notNullable();
   });
 
 export const down = async (knex) => knex.schema.dropTableIfExists("authors");

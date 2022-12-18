7d2101e8978e037192e312f9e99350f47edef4dd
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2022-12-17 19:24:48 -0800

    created add book card and linked to button

--- client/src/lib/Library_table.svelte
+++ client/src/lib/Library_table.svelte
@@ -15,26 +15,64 @@
     { read_status:true, title: 'Amerika', author: 'Franz Kafka', edition: 'undetermined', ISBN: '978-0-8052-1064-4'},
     { read_status:false, title: 'This Indian Country', author: 'Frederick E. Hoxie', edition: 'undetermined', ISBN: '978-1-59420-365-7'}
 	];
+
+  let inputTitle='title';
+  let inputAuthor='author';
+  let inputEdition='edition';
+  let inputISBN='ISBN';
+
+  export let libraryTableVisible=true;
+
+  function handleAddBookClick() {
+    libraryTableVisible=false;
+    libraryTableVisible=libraryTableVisible;
+  }
+  function handleFinalAddBookClick() {
+    libraryTableVisible=true;
+    libraryTableVisible=libraryTableVisible;
+  }
+
 </script>
 
-<span class="lib_table_span">
-  <h2 style="color:black; text-align:left;">My Library</h2>
-  <div class='filter_menu'>
-    <LibFilterMenu></LibFilterMenu>
-  </div>
-  <div class="button_colnames">
-    <Button add_book_button button_text={"+"}></Button>
-    <p>Title</p>
-    <p>Author</p>
-    <p>Edition</p>
-    <p>ISBN</p>
-  </div>
-  <div class="grid-container">
-    {#each books as { read_status, title, author, edition, ISBN }, i}
-      <BookRow read_status={read_status} title={title} author={author} edition={edition} ISBN={ISBN} ></BookRow>
-    {/each}
-  </div>
-</span>
+<div class="lib_table_span">
+{#if libraryTableVisible}
+  
+    <h2 style="color:black; text-align:left;">My Library</h2>
+    <div class='filter_menu'>
+      <LibFilterMenu></LibFilterMenu>
+    </div>
+    <div class="button_colnames">
+      <div class="addBookButton">
+        <Button add_book_button button_text={"+"} on:click={handleAddBookClick}></Button>
+      </div>
+      <p>Title</p>
+      <p>Author</p>
+      <p>Edition</p>
+      <p>ISBN</p>
+    </div>
+    <div class="grid-container">
+      {#each books as { read_status, title, author, edition, ISBN }, i}
+        <BookRow read_status={read_status} title={title} author={author} edition={edition} ISBN={ISBN} ></BookRow>
+      {/each}
+    </div>
+  
+{:else}
+    <div class="card">
+      <div class=cardP>
+          <p>Add a book to your library</p>
+      </div>
+      <div class="inputs">
+        <input bind:value={inputTitle}>
+        <input bind:value={inputAuthor}>
+        <input bind:value={inputEdition}>
+        <input bind:value={inputISBN}>
+      </div>
+      <div class="buttons" style="margin-top: 20px;">
+          <Button button_text={"Add this book"} orange_button on:click={handleFinalAddBookClick}></Button>
+      </div>
+    </div>
+{/if}
+</div>
 
 <style>
   .lib_table_span{
@@ -51,13 +89,8 @@
     grid-template-columns: 1fr 4fr 3fr 1fr 3fr;
     grid-template-rows: auto;
     justify-items: start;
+    align-items: bottom;
     color: grey;
-  }
-  .column_names{
-    display: grid;
-    grid-template-columns: 2fr 2fr 1fr 2fr;
-    color: grey;
-    text-align: left;
   }
   .grid-container{
     --table-width: 900px;
@@ -75,7 +108,34 @@
     width: var(--table-width);
     height: auto;
     color: rgb(86, 86, 86);
-    font-size: 15px;
+    font-size: 15px;  
     font-weight: medium;
   }
+  .card{
+    justify-self: center;
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: repeat(3, 1fr);
+    width: 400px;
+    height: 300px;
+    border-radius: 30px;
+    border: 1px black solid;
+    color: rgb(75, 75, 75);
+  }
+  .card p{
+    line-height: 1.1;
+    font-size: 40px;
+    font-weight: bold;
+    color: black;
+  }
+  input{
+    background-color: white;
+    border-top-color: none;
+    border-color: grey;
+    border-width: 0.5px;
+    color: grey;
+    margin-bottom: 20px;
+    margin-left: 10px;
+    margin-right: 10px;
+    }
 </style>

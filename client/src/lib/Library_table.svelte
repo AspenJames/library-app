2079722cc94c579c48c5b8b418fa2683458e34af
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2022-12-17 19:29:23 -0800

    add shadow to add book card

--- client/src/lib/Library_table.svelte
+++ client/src/lib/Library_table.svelte
@@ -119,8 +119,8 @@
     width: 400px;
     height: 300px;
     border-radius: 30px;
-    border: 1px black solid;
     color: rgb(75, 75, 75);
+    box-shadow: 0px 0px 15px 0px rgb(185, 185, 185);
   }
   .card p{
     line-height: 1.1;

ee3851a18d1e100c0ff079a253aea130a687b0fc
Aspen James <hello@aspenjames.dev>
2022-12-17 20:48:07 -0800

    Add error middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { version } from './constants.js';
+import { errorHandler, errorLogger } from './error.js';
 
 import bookRouter from './routes/bookRouter.js';
 import metaRouter from './routes/metaRouter.js';
@@ -18,5 +19,8 @@ app.use((_, res, next) => {
 // Mount routers
 app.use('/', metaRouter);
 app.use('/books', bookRouter);
+
+app.use(errorLogger);
+app.use(errorHandler);
 
 export default app;
